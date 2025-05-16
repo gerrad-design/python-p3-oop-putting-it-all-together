8a7cf6f9ee6c572401e126d9040ddce1cb3c6628
gerrad <gerrad.meitamei@student.moringaschool.com>
2025-05-16 14:33:38 +0300

    initial commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title=None, page_count=None):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self.page_count = page_count
+
+        self.title = title
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand=None, size=None):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        else:
+            self.size = size
+
+        self.brand = brand
+        self.condition = None
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
